f7a9fc65401f620d9fada3e4a2bdc1fedd00d6b4
unknown <famouszegenee@gmail.com>
2023-04-20 18:51:57 +0100

    correction of the error made by including runApp in the main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main(runApp){
-  runApp(Calculator());
+void main(){
+  runApp(const Calculator());
 }
 
 class Calculator extends StatelessWidget {
@@ -9,6 +9,25 @@ class Calculator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Calculator',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
   }
 }
